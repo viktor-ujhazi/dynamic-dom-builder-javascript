16e57648474be6a38751b03c3a44f2790da6370b
viktor-ujhazi <viktor.ujhazi@gmail.com>
2020-05-22 10:24:05 +0200

    init code

---
+++ index.js
@@ -0,0 +1,132 @@
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+let usersDivEl;
+let postsDivEl;
+let loadButtonEl;
+
+function createPostsList(posts) {
+    const ulEl = document.createElement('ul');
+
+    for (let i = 0; i < posts.length; i++) {
+        const post = posts[i];
+
+        // creating paragraph
+        const strongEl = document.createElement('strong');
+        strongEl.textContent = post.title;
+
+        const pEl = document.createElement('p');
+        pEl.appendChild(strongEl);
+        pEl.appendChild(document.createTextNode(`: ${post.body}`));
+
+        // creating list item
+        const liEl = document.createElement('li');
+        liEl.appendChild(pEl);
+
+        ulEl.appendChild(liEl);
+    }
+
+    return ulEl;
+}
+
+function onPostsReceived() {
+    postsDivEl.style.display = 'block';
+
+    const text = this.responseText;
+    const posts = JSON.parse(text);
+
+    const divEl = document.getElementById('posts-content');
+    while (divEl.firstChild) {
+        divEl.removeChild(divEl.firstChild);
+    }
+    divEl.appendChild(createPostsList(posts));
+}
+
+function onLoadPosts() {
+    const el = this;
+    const userId = el.getAttribute('data-user-id');
+
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onPostsReceived);
+    xhr.open('GET', BASE_URL + '/posts?userId=' + userId);
+    xhr.send();
+}
+
+function createUsersTableHeader() {
+    const idTdEl = document.createElement('td');
+    idTdEl.textContent = 'Id';
+
+    const nameTdEl = document.createElement('td');
+    nameTdEl.textContent = 'Name';
+
+    const trEl = document.createElement('tr');
+    trEl.appendChild(idTdEl);
+    trEl.appendChild(nameTdEl);
+
+    const theadEl = document.createElement('thead');
+    theadEl.appendChild(trEl);
+    return theadEl;
+}
+
+function createUsersTableBody(users) {
+    const tbodyEl = document.createElement('tbody');
+
+    for (let i = 0; i < users.length; i++) {
+        const user = users[i];
+
+        // creating id cell
+        const idTdEl = document.createElement('td');
+        idTdEl.textContent = user.id;
+
+        // creating name cell
+        const dataUserIdAttr = document.createAttribute('data-user-id');
+        dataUserIdAttr.value = user.id;
+
+        const buttonEl = document.createElement('button');
+        buttonEl.textContent = user.name;
+        buttonEl.setAttributeNode(dataUserIdAttr);
+        buttonEl.addEventListener('click', onLoadPosts);
+
+        const nameTdEl = document.createElement('td');
+        nameTdEl.appendChild(buttonEl);
+
+        // creating row
+        const trEl = document.createElement('tr');
+        trEl.appendChild(idTdEl);
+        trEl.appendChild(nameTdEl);
+
+        tbodyEl.appendChild(trEl);
+    }
+
+    return tbodyEl;
+}
+
+function createUsersTable(users) {
+    const tableEl = document.createElement('table');
+    tableEl.appendChild(createUsersTableHeader());
+    tableEl.appendChild(createUsersTableBody(users));
+    return tableEl;
+}
+
+function onUsersReceived() {
+    loadButtonEl.remove();
+
+    const text = this.responseText;
+    const users = JSON.parse(text);
+
+    const divEl = document.getElementById('users-content');
+    divEl.appendChild(createUsersTable(users));
+}
+
+function onLoadUsers() {
+    const xhr = new XMLHttpRequest();
+    xhr.addEventListener('load', onUsersReceived);
+    xhr.open('GET', BASE_URL + '/users');
+    xhr.send();
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    usersDivEl = document.getElementById('users');
+    postsDivEl = document.getElementById('posts');
+    loadButtonEl = document.getElementById('load-users');
+    loadButtonEl.addEventListener('click', onLoadUsers);
+});
